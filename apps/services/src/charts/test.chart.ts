d0c04bbed01324a85bcaa50bf4b3a20f5d21753e
David <dfain@wemaintain.io>
2021-04-30 17:47:53 +0200

    feat: switch to cdk8s

---
+++ apps/services/src/charts/test.chart.ts
@@ -0,0 +1,37 @@
+import { Construct } from 'constructs';
+import { App, Chart, ChartProps } from 'cdk8s';
+import { KubeDeployment, KubeService, IntOrString } from '@homeapi/k8s';
+
+
+export class MyChart extends Chart {
+  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+    super(scope, id, props);
+
+    const label = { app: 'hello-k8s' };
+
+    new KubeDeployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.7',
+                ports: [ { containerPort: 8080 } ]
+              }
+            ]
+          }
+        }
+      }
+    });
+  }
+}
+
+const app = new App();
+new MyChart(app, 'helloworldtest');
+app.synth();
