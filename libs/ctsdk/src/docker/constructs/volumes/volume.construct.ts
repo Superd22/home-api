c023c2717ec399a253339d13ab1bd4e1aba0fca4
David FAIN <superd001@gmail.com>
2022-10-16 17:10:41 +0200

    feat: volume to service

--- libs/ctsdk/src/docker/constructs/volumes/volume.construct.ts
+++ libs/ctsdk/src/docker/constructs/volumes/volume.construct.ts
@@ -1,9 +1,19 @@
 import { Compose } from "../compose.construct";
 import { Construct } from "../../../sdk";
-import { DefinitionsVolume } from "../../compose-v3";
+import { DefinitionsService, DefinitionsVolume } from "../../compose-v3";
+type Unpacked<T> = T extends (infer U)[] ? U : T;
 
 export class Volume extends Construct<VolumeProps, Compose> {
-
+  /**
+   * Bind this volume in a service to the given path inside the container
+   */
+  public toService(opts: { path: string }, scope: Compose = this.scope): Unpacked<DefinitionsService['volumes']> {
+    return {
+      type: 'volume',
+      source: this.id(scope),
+      target: opts.path,
+    }
+  }
 }
 
 export interface VolumeProps extends DefinitionsVolume { }
