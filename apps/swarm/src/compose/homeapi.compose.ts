4370240fafc95c37279f4d9f95a4d9aeaccd1ca8
David FAIN <superd001@gmail.com>
2022-12-24 21:08:05 +0100

    deploy(homeapi): add pg

--- apps/swarm/src/compose/homeapi.compose.ts
+++ apps/swarm/src/compose/homeapi.compose.ts
@@ -1,12 +1,15 @@
-import { Compose } from '@homeapi/ctsdk';
+import { Compose, Network, Service, Volume } from '@homeapi/ctsdk';
 import { Injectable } from '@nestjs/common';
 import { AvailableNodes, NodeSelector } from '../charts/node-selector';
 import { keyValueFromConfig } from '../charts/utils/kv-from-config.util';
+import { Config } from '../config.encrypted';
 import { WebServiceFactory } from '../services/web-service/web-service.factory';
 import { SwarmApp } from '../swarm.service';
 
 @Injectable()
 export class HomeAPI extends Compose {
+
+  protected network = new Network(this, 'homeapi');
 
   protected api = this.web.webService(this, 'api', {
     web: {
@@ -20,16 +23,43 @@ export class HomeAPI extends Compose {
           "dockupdater.latest": true,
           "dockupdater.enable": true,
         })
-      }
+      },
+      networks: {
+        ...this.network.toService()
+      },
     }
   })
 
+  protected readonly postgres = new Service(this, 'pg', {
+    image: 'postgres:15.1',
+    volumes: [
+      new Volume(this, 'pg-data').toService({ path: '/var/lib/postgresql/data' })
+    ],
+    healthcheck: {
+      test: ["CMD-SHELL", "pg_isready -U postgres"],
+      interval: '5s',
+      timeout: '5s',
+      retries: 5
+    },
+    environment: keyValueFromConfig({
+      PGDATA: '/var/lib/postgresql/data/pgdata',
+      POSTGRES_PASSWORD: this.config.homeapi.pg.password,
+      POSTGRES_USER: this.config.homeapi.pg.user,
+      POSTGRES_DB: 'homeapi'
+    }),
+    networks: {
+      ...this.network.toService()
+    }
+  })
+  
   constructor(
     protected readonly app: SwarmApp,
     protected readonly web: WebServiceFactory,
+    protected readonly config: Config,
   ) {
     super(app, HomeAPI.name, { version: '3.6', name: null });
     new NodeSelector(this.api, AvailableNodes.HomeAPI)
+    new NodeSelector(this.postgres, AvailableNodes.Galactica)
   }
 
 }
